31c160cea58ef4fc80495f14461598fa783f04be
Administrator <h3584935@gmail.com>
2017-11-26 19:17:00 +0800

    Add get started of ltp

--- ltp/ltp-start.py
+++ ltp/ltp-start.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from pyltp import Segmentor
+
+modelPath='./data/ltp_data_v3.4.0/cws.model'
+segmentor=Segmentor()
+segmentor.load(modelPath)
+
+sent='在包含問題的所有解的解空間樹中，按照深度優先搜尋的策略，從根節點出發深度優先搜尋解空間樹'
+words=segmentor.segment(sent)
+print(' | '.join(words))
